9bbc4ce94a075bc1755e1cc0ff09104a002ab40a
Opemiplenty <opemikoga@gmail.com>
2025-08-17 06:20:26 +0100

    added menu-bar

---
+++ script.js
@@ -0,0 +1,35 @@
+/*Features*/
+  function activateCard(element) {
+    // Remove "active" from all cards
+    document.querySelectorAll('.card').forEach(card => {
+      card.classList.remove('active');
+    });
+
+    // Add "active" to clicked card
+    element.classList.add('active');
+  }
+
+
+  /*Testimonials*/
+  document.addEventListener('DOMContentLoaded', () => {
+  const cards = document.querySelectorAll('.testimonial-card');
+
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      // First, remove the 'active' class from all cards
+      cards.forEach(c => c.classList.remove('active'));
+
+      // Then, add the 'active' class to the clicked card
+      card.classList.add('active');
+    });
+  });
+});
+  /*Menu-bar*/
+    document.addEventListener('DOMContentLoaded', () => {
+  const hamburgerBtn = document.getElementById('hamburger-btn');
+  const navMenu = document.getElementById('mobile-menu');
+
+  hamburgerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+});
