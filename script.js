5e66a33036b6c78335e3cd2f8d4efbb5d0a34891
Opemiplenty <opemikoga@gmail.com>
2025-08-19 22:13:33 +0100

    completed the project

--- script.js
+++ script.js
@@ -25,11 +25,13 @@
   });
 });
   /*Menu-bar*/
+  document.addEventListener("DOMContentLoaded", () => {
   const hamMenu = document.querySelector('.ham-menu');
   const offScreenMenu = document.querySelector('.off-screen-menu');
 
   hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active')
+  });
   });
    
